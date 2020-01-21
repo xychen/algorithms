a51d716951bb75673539414407f75c2de2667b31
chenxingyu <chenxingyu5@100tal.com>
2020-01-21 23:41:58 +0800

    添加listnode的方法

--- kit/listnode.go
+++ kit/listnode.go
@@ -20,3 +20,35 @@ func (l *ListNode) Vals() []int {
 	}
 	return res
 }
+
+//根据整型数组创建链表
+func MakeListNode(vals []int) *ListNode {
+	if len(vals) == 0 {
+		return nil
+	}
+
+	res := &ListNode{}
+	p := res
+	for _, v := range vals {
+		p.Next = &ListNode{Val: v}
+		p = p.Next
+	}
+	return res.Next
+}
+
+//判断值是否相等
+func IsValsEqual(l1 *ListNode, l2 *ListNode) bool {
+	p1, p2 := l1, l2
+	for p1 != nil && p2 != nil {
+		if p1.Val != p2.Val {
+			return false
+		}
+		p1 = p1.Next
+		p2 = p2.Next
+	}
+	//长度不同
+	if p1 != nil || p2 != nil {
+		return false
+	}
+	return true
+}
