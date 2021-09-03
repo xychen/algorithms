ca7c9e86d0ac6e5f5b0b9cebe21775d81119a982
xychen <cxybjfu@126.com>
2021-09-03 12:37:07 +0800

    填充每个节点的下一个右侧节点指针

---
+++ leetcode/0116.populating-next-right-pointers-in-each-node/0116.populating-next-right-pointers-in-each-node.go
@@ -0,0 +1,28 @@
+package problem0116
+
+//填充每个节点的下一个右侧节点指针
+
+type Node struct {
+	Val   int
+	Left  *Node
+	Right *Node
+	Next  *Node
+}
+
+func connect(root *Node) *Node {
+	if root == nil {
+		return root
+	}
+	connectTwoNode(root.Left, root.Right)
+	return root
+}
+
+func connectTwoNode(node1, node2 *Node) {
+	if node1 == nil || node2 == nil {
+		return
+	}
+	node1.Next = node2
+	connectTwoNode(node1.Left, node1.Right)
+	connectTwoNode(node2.Left, node2.Right)
+	connectTwoNode(node1.Right, node2.Left)
+}
