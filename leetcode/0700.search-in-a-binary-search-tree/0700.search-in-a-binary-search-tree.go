2456b0639a92c43752a37ac842c090eb0635128a
xychen <cxybjfu@126.com>
2021-09-06 21:57:07 +0800

    二叉搜索树中的搜索

---
+++ leetcode/0700.search-in-a-binary-search-tree/0700.search-in-a-binary-search-tree.go
@@ -0,0 +1,22 @@
+package problem0700
+
+// 二叉搜索树中的搜索
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func searchBST(root *TreeNode, val int) *TreeNode {
+	if root == nil {
+		return nil
+	}
+	if val == root.Val {
+		return root
+	} else if val < root.Val {
+		return searchBST(root.Left, val)
+	} else {
+		return searchBST(root.Right, val)
+	}
+}
