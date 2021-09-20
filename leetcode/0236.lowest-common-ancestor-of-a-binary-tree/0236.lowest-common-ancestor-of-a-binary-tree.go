81a58c6e798c134850594131d1bc92edb4258e2b
xychen <cxybjfu@126.com>
2021-09-20 11:49:24 +0800

    最近公共祖先

---
+++ leetcode/0236.lowest-common-ancestor-of-a-binary-tree/0236.lowest-common-ancestor-of-a-binary-tree.go
@@ -0,0 +1,29 @@
+package problem0236
+
+// 最近公共祖先
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
+	if root == nil {
+		return nil
+	}
+	if root == p || root == q {
+		return root
+	}
+	left := lowestCommonAncestor(root.Left, p, q)
+	right := lowestCommonAncestor(root.Right, p, q)
+	if left != nil && right != nil {
+		return root
+	} else {
+		if left == nil {
+			return right
+		} else {
+			return left
+		}
+	}
+}
