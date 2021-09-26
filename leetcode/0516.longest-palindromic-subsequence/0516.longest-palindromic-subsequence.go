5917f2c46ab7805ce2275d5eacb2d008029c7e33
xychen <cxybjfu@126.com>
2021-09-26 22:23:57 +0800

    最长回文子序列

---
+++ leetcode/0516.longest-palindromic-subsequence/0516.longest-palindromic-subsequence.go
@@ -0,0 +1,29 @@
+package problem0516
+
+// 最长回文子序列
+
+func longestPalindromeSubseq(s string) int {
+	n := len(s)
+	dp := make([][]int, n)
+	for i := 0; i < n; i++ {
+		dp[i] = make([]int, n)
+		dp[i][i] = 1
+	}
+	for i := n - 2; i >= 0; i-- {
+		for j := i + 1; j < n; j++ {
+			if s[i] == s[j] {
+				dp[i][j] = dp[i+1][j-1] + 2
+			} else {
+				dp[i][j] = max(dp[i+1][j], dp[i][j-1])
+			}
+		}
+	}
+	return dp[0][n-1]
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
