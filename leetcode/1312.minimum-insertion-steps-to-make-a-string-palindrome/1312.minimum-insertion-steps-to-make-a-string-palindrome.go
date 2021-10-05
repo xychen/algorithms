fa8fedfde07bc4bd424a35a001a06373c6580f02
xychen <cxybjfu@126.com>
2021-10-05 22:11:21 +0800

    让字符串成为回文串的最少插入次数

---
+++ leetcode/1312.minimum-insertion-steps-to-make-a-string-palindrome/1312.minimum-insertion-steps-to-make-a-string-palindrome.go
@@ -0,0 +1,29 @@
+package problem1312
+
+// 让字符串成为回文串的最少插入次数
+
+func minInsertions(s string) int {
+	n := len(s)
+	dp := make([][]int, n)
+	for i := 0; i < n; i++ {
+		dp[i] = make([]int, n)
+	}
+
+	for i := n - 2; i >= 0; i-- {
+		for j := i + 1; j < n; j++ {
+			if s[i] == s[j] {
+				dp[i][j] = dp[i+1][j-1]
+			} else {
+				dp[i][j] = min(dp[i+1][j], dp[i][j-1]) + 1
+			}
+		}
+	}
+	return dp[0][n-1]
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
