49ba7672bcc85fb0368423ca60d8def170b16cc1
xychen <cxybjfu@126.com>
2021-10-10 17:07:41 +0800

    全排列2

--- leetcode/0047.permutations-ii/0047.permutations-ii.go
+++ leetcode/0047.permutations-ii/0047.permutations-ii.go
@@ -5,35 +5,34 @@ package problem0047
 
 import "sort"
 
-var res [][]int
-var visted map[int]bool
-
 func permuteUnique(nums []int) [][]int {
-	res = make([][]int, 0)
-	visted = make(map[int]bool)
-	path := make([]int, 0)
+	n := len(nums)
 	sort.Ints(nums)
-	solve(nums, path)
+	res := make([][]int, 0)
+	visted := make([]bool, n)
+	path := make([]int, 0)
+	var solve func(path []int)
+	solve = func(path []int) {
+		if len(nums) == len(path) {
+			tmp := make([]int, len(path))
+			copy(tmp, path)
+			res = append(res, tmp)
+			return
+		}
+		for i := 0; i < len(nums); i++ {
+			// 要解决重复问题，我们只要设定一个规则，保证在填第idx 个数的时候重复数字只会被填入一次即可。
+			// 而在本题解中，我们选择对原数组排序，保证相同的数字都相邻，然后每次填入的数一定是这个数所在重复数集合中「从左往右第一个未被填过的数字」，
+			// 即如下的判断条件：i > 0 && nums[i] == nums[i-1] && !visted[i-1]  , !visted[i-1] 表示i-1没使用，那i肯定不是第一个
+			if visted[i] || (i > 0 && nums[i] == nums[i-1] && !visted[i-1]) {
+				continue
+			}
+			visted[i] = true
+			path = append(path, nums[i])
+			solve(path)
+			path = path[:len(path)-1]
+			visted[i] = false
+		}
+	}
+	solve(path)
 	return res
-}
-func solve(nums []int, path []int) {
-	if len(nums) == len(path) {
-		tmp := make([]int, len(nums))
-		copy(tmp, path)
-		res = append(res, tmp)
-	}
-	for i := 0; i < len(nums); i++ {
-		if v, ok := visted[i]; ok && v {
-			continue
-		}
-		if i > 0 && nums[i] == nums[i-1] && visted[i-1] == false {
-			continue
-		}
-		path = append(path, nums[i])
-		visted[i] = true
-		solve(nums, path)
-
-		visted[i] = false
-		path = path[:len(path)-1]
-	}
 }
