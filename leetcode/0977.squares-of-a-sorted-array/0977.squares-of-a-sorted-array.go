4ebbea6dba5f57de8d0e40b67c1a5663048b4f51
xychen <cxybjfu@126.com>
2021-10-26 23:16:30 +0800

    优化0977

--- leetcode/0977.squares-of-a-sorted-array/0977.squares-of-a-sorted-array.go
+++ leetcode/0977.squares-of-a-sorted-array/0977.squares-of-a-sorted-array.go
@@ -4,39 +4,18 @@ package problem0977
 // 给你一个按 非递减顺序 排序的整数数组 nums，返回 每个数字的平方 组成的新数组，要求也按 非递减顺序 排序。
 
 func sortedSquares(nums []int) []int {
-	// 负数结束的位置
-	flag := -1
-	for i, num := range nums {
-		if num < 0 {
-			flag = i
-		}
-		nums[i] = num * num
-	}
+	left, right, index := 0, len(nums)-1, len(nums)-1
 	res := make([]int, len(nums))
-	left := flag
-	right := flag + 1
-	i := 0
-	for left >= 0 || right < len(nums) {
-		if left >= 0 && right < len(nums) {
-			if nums[left] < nums[right] {
-				res[i] = nums[left]
-				left--
-			} else {
-				res[i] = nums[right]
-				right++
-			}
-			i++
-			continue
+
+	for left <= right {
+		if nums[left]*nums[left] > nums[right]*nums[right] {
+			res[index] = nums[left] * nums[left]
+			left++
+		} else {
+			res[index] = nums[right] * nums[right]
+			right--
 		}
-		if left >= 0 {
-			res[i] = nums[left]
-			left--
-		}
-		if right < len(nums) {
-			res[i] = nums[right]
-			right++
-		}
-		i++
+		index--
 	}
 	return res
 }
