b2cacc0a1ebdb2ceb66ec1491c9d85749b234112
xychen <cxybjfu@126.com>
2022-01-09 21:40:18 +0800

    完全背包问题

--- leetcode/0377.combination-sum-iv/0377.combination-sum-iv.go
+++ leetcode/0377.combination-sum-iv/0377.combination-sum-iv.go
@@ -4,6 +4,9 @@ package problem0377
 // 给你一个由 不同 整数组成的数组 nums ，和一个目标整数 target 。请你从 nums 中找出并返回总和为 target 的元素组合的个数。
 
 // 方法一：动态规划 （零钱兑换问题）
+// 本题求排列数
+// 如果求组合数，外层for循环遍历物品、内层for循环遍历背包
+// 如果求排列数，外层for循环遍历背包、内层for循环遍历物品
 func combinationSum4(nums []int, target int) int {
 	// dp[i] 表示和为i的元素组合个数
 	dp := make([]int, target+1)
@@ -17,32 +20,4 @@ func combinationSum4(nums []int, target int) int {
 		}
 	}
 	return dp[target]
-}
-
-// 方法二： 回溯 + 记忆化搜索
-var dp map[int]int
-
-func combinationSum4_2(nums []int, target int) int {
-	dp = make(map[int]int)
-	return solve(nums, target)
-}
-
-func solve(nums []int, target int) int {
-	if target == 0 {
-		return 1
-	}
-	if target < 0 {
-		return 0
-	}
-	if v, ok := dp[target]; ok {
-		return v
-	}
-	res := 0
-	for _, num := range nums {
-		//最选择
-		res += solve(nums, target-num)
-		//回退
-	}
-	dp[target] = res
-	return res
 }
--- leetcode/0518.coin-change-2/0518.coin-change-2.go
+++ leetcode/0518.coin-change-2/0518.coin-change-2.go
@@ -1,4 +1,13 @@
-//
-//Author: 陈星宇 <chenxingyu5@100tal.com>
-//Date: 2022-01-05 00:29:38
+package problem0518
 
+func change(amount int, coins []int) int {
+	// dp[j]的含义：兑换金额为 amount 的时候，有多少种组合，等于所有 dp[j-coin] 之和
+	dp := make([]int, amount+1)
+	dp[0] = 1
+	for _, coin := range coins {
+		for j := coin; j <= amount; j++ {
+			dp[j] += dp[j-coin]
+		}
+	}
+	return dp[amount]
+}
