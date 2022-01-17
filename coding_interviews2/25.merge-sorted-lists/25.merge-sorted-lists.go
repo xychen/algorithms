d4f2e3d8b89e647cbe9c88c1ac3d81cff554dfd7
xychen <cxybjfu@126.com>
2022-01-17 23:56:36 +0800

    merge-sorted-lists

---
+++ coding_interviews2/25.merge-sorted-lists/25.merge-sorted-lists.go
@@ -0,0 +1,37 @@
+package problem25
+
+// 剑指 Offer 25. 合并两个排序的链表
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
+	if l1 == nil {
+		return l2
+	}
+	if l2 == nil {
+		return l1
+	}
+
+	newhead := &ListNode{}
+	p := newhead
+	for l1 != nil && l2 != nil {
+		if l1.Val < l2.Val {
+			p.Next = l1
+			l1 = l1.Next
+		} else {
+			p.Next = l2
+			l2 = l2.Next
+		}
+		p = p.Next
+	}
+	if l1 != nil {
+		p.Next = l1
+	}
+	if l2 != nil {
+		p.Next = l2
+	}
+	return newhead.Next
+}
