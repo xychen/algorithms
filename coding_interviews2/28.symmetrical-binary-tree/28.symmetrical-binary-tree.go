b600977e67e22ceb607b408655c933b13c1e7f1a
xychen <cxybjfu@126.com>
2022-01-23 21:33:48 +0800

    对称的二叉树

---
+++ coding_interviews2/28.symmetrical-binary-tree/28.symmetrical-binary-tree.go
@@ -0,0 +1,29 @@
+package problem28
+
+// 剑指 Offer 28. 对称的二叉树
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func isSymmetric(root *TreeNode) bool {
+	if root == nil {
+		return true
+	}
+	return checkSymmetric(root.Left, root.Right)
+}
+
+func checkSymmetric(root1 *TreeNode, root2 *TreeNode) bool {
+	if root1 == nil && root2 == nil {
+		return true
+	}
+	if root1 == nil || root2 == nil {
+		return false
+	}
+	if root1.Val != root2.Val {
+		return false
+	}
+	return checkSymmetric(root1.Left, root2.Right) && checkSymmetric(root1.Right, root2.Left)
+}
