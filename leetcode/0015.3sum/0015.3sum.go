53332a8d7d79613383647349be67a1521c7bc4e2
xychen <cxybjfu@126.com>
2022-02-14 00:04:24 +0800

    3sum通用解法

--- leetcode/0015.3sum/0015.3sum.go
+++ leetcode/0015.3sum/0015.3sum.go
@@ -12,44 +12,48 @@ import "sort"
 func threeSum(nums []int) [][]int {
 	// 排序
 	sort.Ints(nums)
+	return nSum(nums, 3, 0, 0)
+}
+
+// n表示nsum
+func nSum(nums []int, n, start, target int) [][]int {
 	res := make([][]int, 0)
-	for i := 0; i < len(nums); i++ {
+	// twosum 是base case
+	if n == 2 {
+		l, r := start, len(nums)-1
+		for l < r {
+			lval, rval := nums[l], nums[r]
+			sum := lval + rval
+			if target < sum {
+				for l < r && nums[r] == rval {
+					r--
+				}
+			} else if target > sum {
+				for l < r && nums[l] == lval {
+					l++
+				}
+			} else {
+				res = append(res, []int{lval, rval})
+				for l < r && nums[r] == rval {
+					r--
+				}
+				for l < r && nums[l] == lval {
+					l++
+				}
+			}
+		}
+		return res
+	}
+
+	for i := start; i < len(nums); i++ {
 		num := nums[i]
-		tmpres := twoSum(nums, i+1, -num)
+		tmpres := nSum(nums, n-1, i+1, target-num)
 		for _, item := range tmpres {
 			item = append(item, num)
 			res = append(res, item)
 		}
-		//避免重复
-		for i < len(nums)-1 && nums[i+1] == num {
+		for i < len(nums)-1 && num == nums[i+1] {
 			i++
-		}
-	}
-	return res
-}
-
-func twoSum(nums []int, start, target int) [][]int {
-	res := make([][]int, 0)
-	l, r := start, len(nums)-1
-	for l < r {
-		lval, rval := nums[l], nums[r]
-		if lval+rval == target {
-			tmp := []int{lval, rval}
-			res = append(res, tmp)
-			for l < r && rval == nums[r] {
-				r--
-			}
-			for l < r && lval == nums[l] {
-				l++
-			}
-		} else if lval+rval > target {
-			for l < r && rval == nums[r] {
-				r--
-			}
-		} else if lval+rval < target {
-			for l < r && lval == nums[l] {
-				l++
-			}
 		}
 	}
 	return res
