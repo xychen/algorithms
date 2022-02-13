dc1511e967466ada5471cd837ad6cde162bc29a3
xychen <cxybjfu@126.com>
2022-02-14 00:06:11 +0800

    四数相加 II

--- leetcode/0454.4sum-ii/0454.4sum-ii.go
+++ leetcode/0454.4sum-ii/0454.4sum-ii.go
@@ -1,5 +1,11 @@
 package problem0454
 
+// 454. 四数相加 II
+// 给你四个整数数组 nums1、nums2、nums3 和 nums4 ，数组长度都是 n ，请你计算有多少个元组 (i, j, k, l) 能满足：
+
+// 0 <= i, j, k, l < n
+// nums1[i] + nums2[j] + nums3[k] + nums4[l] == 0
+
 func fourSumCount(nums1 []int, nums2 []int, nums3 []int, nums4 []int) int {
 	ht := make(map[int]int, 0)
 	for _, n1 := range nums1 {
